50227e3fec761904e5a049dd6605243643c914f1
Gergely Nemeth <gergelyke@users.noreply.github.com>
2018-03-29 08:33:28 -0700

    feat(version): add node 8.11.0

--- debian/versions.js
+++ debian/versions.js
@@ -1,12 +1,12 @@
 module.exports = [
   {
-    nodeVersion: '8.10.0',
+    nodeVersion: '8.11.0',
     additionalFiles: [
       'base-entrypoint.sh'
     ]
   },
   {
-    nodeVersion: '9.7.1',
+    nodeVersion: '9.10.0',
     additionalFiles: [
       'base-entrypoint.sh'
     ]
