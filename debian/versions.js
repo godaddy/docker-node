ecd67dc8d94f21549d00c530575add5945f7ce57
JacopoDaeli <jacopo.daeli@gmail.com>
2018-09-24 17:39:15 -0400

    feat(node): version 10.11.0 and 8.12.0

--- debian/versions.js
+++ debian/versions.js
@@ -1,12 +1,12 @@
 module.exports = [
   {
-    nodeVersion: '8.11.3',
+    nodeVersion: '8.12.0',
     additionalFiles: [
       'base-entrypoint.sh'
     ]
   },
   {
-    nodeVersion: '10.5.0',
+    nodeVersion: '10.11.0',
     additionalFiles: [
       'base-entrypoint.sh'
     ]
